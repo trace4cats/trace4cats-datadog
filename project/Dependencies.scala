56d72faa2fb0041ea716e02e618941e6bdbffa0e
Yannick Heiber <ybasket42@googlemail.com>
2021-07-30 11:11:59 +0200

    Add Scala 3 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,10 @@ object Dependencies {
   object Versions {
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
+    val scala3 = "3.0.1"
 
-    val trace4cats = "0.12.0-RC2"
-    val trace4catsExporterHttp = "0.12.0-RC2"
+    val trace4cats = "0.12.0-RC2+18-2de6b68e"
+    val trace4catsExporterHttp = "0.12.0-RC2+4-69bfcea6"
 
     val circe = "0.14.1"
     val http4s = "0.23.0-RC1"
@@ -21,10 +22,10 @@ object Dependencies {
   lazy val trace4catsTestkit = "io.janstenpickle"        %% "trace4cats-testkit"         % Versions.trace4cats
   lazy val trace4catsExporterHttp = "io.janstenpickle"   %% "trace4cats-exporter-http"   % Versions.trace4catsExporterHttp
 
-  lazy val circeGeneric = "io.circe"        %% "circe-generic-extras" % Versions.circe
-  lazy val circeParser = "io.circe"         %% "circe-parser"         % Versions.circe
-  lazy val http4sCirce = "org.http4s"       %% "http4s-circe"         % Versions.http4s
-  lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client"  % Versions.http4s
+  lazy val circeGeneric = "io.circe"        %% "circe-generic"       % Versions.circe
+  lazy val circeParser = "io.circe"         %% "circe-parser"        % Versions.circe
+  lazy val http4sCirce = "org.http4s"       %% "http4s-circe"        % Versions.http4s
+  lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % Versions.http4s
 
   lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
