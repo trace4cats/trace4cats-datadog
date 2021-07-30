72cdb9742bb034259feaff614631e2b46bd3604e
Yannick Heiber <ybasket42@googlemail.com>
2021-07-30 08:06:00 +0200

    Update project/Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala213 = "2.13.6"
     val scala3 = "3.0.1"
 
-    val trace4cats = "0.12.0-RC2+17-d73c7ff3"
+    val trace4cats = "0.12.0-RC2+18-2de6b68e"
     val trace4catsExporterHttp = "0.12.0-RC2+4-69bfcea6"
 
     val circe = "0.14.1"
