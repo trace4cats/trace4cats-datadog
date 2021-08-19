f444c4badacfa926d012011383a7131d63dae398
Scala Steward <me@scala-steward.org>
2021-08-19 06:27:55 +0200

    Reformat with scalafmt 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   lazy val trace4catsKernel = "io.janstenpickle"         %% "trace4cats-kernel"          % Versions.trace4cats
   lazy val trace4catsModel = "io.janstenpickle"          %% "trace4cats-model"           % Versions.trace4cats
   lazy val trace4catsTestkit = "io.janstenpickle"        %% "trace4cats-testkit"         % Versions.trace4cats
-  lazy val trace4catsExporterHttp = "io.janstenpickle"   %% "trace4cats-exporter-http"   % Versions.trace4catsExporterHttp
+  lazy val trace4catsExporterHttp = "io.janstenpickle" %% "trace4cats-exporter-http" % Versions.trace4catsExporterHttp
 
   lazy val circeGeneric = "io.circe"        %% "circe-generic"       % Versions.circe
   lazy val http4sCirce = "org.http4s"       %% "http4s-circe"        % Versions.http4s
