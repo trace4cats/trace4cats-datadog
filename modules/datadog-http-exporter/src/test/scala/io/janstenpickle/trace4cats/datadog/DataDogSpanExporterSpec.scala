f0971027816ea1597e28e67187f7520287570c0b
Jarrod Young <jarrod.y@bluechipfinancial.com>
2021-09-17 17:59:20 -0400

    #616 - Add test

--- modules/datadog-http-exporter/src/test/scala/io/janstenpickle/trace4cats/datadog/DataDogSpanExporterSpec.scala
+++ modules/datadog-http-exporter/src/test/scala/io/janstenpickle/trace4cats/datadog/DataDogSpanExporterSpec.scala
@@ -5,7 +5,7 @@ import cats.effect.unsafe.implicits.global
 import fs2.Chunk
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import io.janstenpickle.trace4cats.model.Batch
+import io.janstenpickle.trace4cats.model.{Batch, CompletedSpan}
 import io.janstenpickle.trace4cats.test.ArbitraryInstances
 import org.scalacheck.Shrink
 import org.scalatest.flatspec.AnyFlatSpec
@@ -20,6 +20,26 @@ class DataDogSpanExporterSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyC
   implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
 
   behavior.of("DataDogSpanExporter")
+
+  it should "construct ids with the DataDog ExternalTraceContext" in forAll { (batch: Batch[Chunk]) =>
+    val batchSpans: List[CompletedSpan] = batch.spans.toList
+    val dataDogSpans: List[DataDogSpan] = DataDogSpan.fromBatch(batch).flatten
+    assert(
+      dataDogSpans.forall(ddSpan =>
+        batchSpans.exists(batchSpan => ddSpan.trace_id == DataDogSpan.traceId(batchSpan.context))
+      )
+    )
+    assert(
+      dataDogSpans.forall(ddSpan =>
+        batchSpans.exists(batchSpan => ddSpan.span_id == DataDogSpan.spanId(batchSpan.context))
+      )
+    )
+    assert(
+      dataDogSpans.forall(ddSpan =>
+        batchSpans.exists(batchSpan => ddSpan.parent_id == DataDogSpan.parentId(batchSpan.context))
+      )
+    )
+  }
 
   it should "send spans to datadog agent without error" in forAll { (batch: Batch[Chunk]) =>
     assertResult(())(DataDogSpanExporter.blazeClient[IO, Chunk]().use(_.exportBatch(batch)).unsafeRunSync())
