74e0d0c170a6b59f8e5f9c9a0102d96dcad1e032
Scala Steward <me@scala-steward.org>
2022-03-31 03:19:09 +0200

    Update trace4cats-exporter-common, ... to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
     val scala213 = "2.13.8"
     val scala3 = "3.1.1"
 
-    val trace4cats = "0.13.0"
-    val trace4catsExporterHttp = "0.13.0"
+    val trace4cats = "0.13.1"
+    val trace4catsExporterHttp = "0.13.1"
 
     val circe = "0.14.1"
     val http4s = "0.23.11"
