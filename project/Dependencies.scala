bbd9d2d293ff034c8aada60776f3fb329477abda
Scala Steward <scala_steward@virtuslab.com>
2023-07-20 21:37:21 +0000

    Update trace4cats-core, ... to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala213 = "2.13.8"
     val scala3 = "3.3.0"
 
-    val trace4cats = "0.14.0"
+    val trace4cats = "0.14.4"
     val trace4catsExporterHttp = "0.14.0"
 
     val circe = "0.14.2"
